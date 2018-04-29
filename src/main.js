f1a4c168978b73f31171c7d6989b1a2c2fd1128e
Dave Dane Pacilan <davedanepacilan3p@gmail.com>
2018-04-29 16:00:11 +0800

    Initialize app

--- src/main.js
+++ src/main.js
@@ -1,15 +1,30 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import VueFire from 'vuefire'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+
+import firebase from './firebase-init'
 import router from './router'
+
+import App from './App'
+
+Vue.use(VueFire)
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+let app
+
+firebase.auth().onAuthStateChanged(auth => {
+  if (!app) {
+    app = new Vue({
+      el: '#app',
+      router,
+      template: '<App/>',
+      components: { App }
+    })
+  }
 })
