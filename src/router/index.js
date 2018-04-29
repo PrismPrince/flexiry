e01ff26d99e37d6f8ac39bebd6587467bc8ec3f5
Dave Dane Pacilan <davedanepacilan3p@gmail.com>
2018-04-29 15:58:06 +0800

    Register routes and add guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Firebase from 'firebase'
+
+// components
+import Welcome from '@/components/Welcome'
+import NotFound from '@/components/Not-Found'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
+  mode: 'history',
   routes: [
     {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Welcome',
+      component: Welcome
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  let user = Firebase.auth().currentUser
+  let hasGuard = to.matched.some(record => record.meta.guard)
+  let guard = null
+
+  if (hasGuard) guard = to.matched.meta.guard
+
+  if (user && guard === 'auth') next()
+  else if (user && guard === 'unauth') next('home')
+  else next()
+})
+
+export default router
