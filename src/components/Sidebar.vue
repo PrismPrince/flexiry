47eef18101c1c0709c056345ef0edd97fbdaaedc
Dave Dane Pacilan <davedanepacilan3p@mail.com>
2018-05-25 10:42:04 +0800

    Add sidebar links

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,16 +5,31 @@
     </md-toolbar>
 
     <md-list>
-          <md-list-item to="/login" v-if="!user">
-            <md-icon>person</md-icon>
-            <span class="md-list-item-text">Login</span>
-          </md-list-item>
+      <md-list-item to="/login" v-if="!user">
+        <md-icon>person</md-icon>
+        <span class="md-list-item-text">Login</span>
+      </md-list-item>
 
-          <md-list-item to="/home" v-if="user">
-            <md-icon>home</md-icon>
-            <span class="md-list-item-text">Home</span>
-          </md-list-item>
+      <md-list-item to="/home" v-if="user">
+        <md-icon>home</md-icon>
+        <span class="md-list-item-text">Home</span>
+      </md-list-item>
+
+      <md-list-item to="/qc-rejection">
+        <md-icon>list</md-icon>
+        <span class="md-list-item-text">QC Rejection</span>
+      </md-list-item>
+
+      <md-list-item md-expand>
+        <md-icon>code</md-icon>
+        <span class="md-list-item-text">Tools and Scripts</span>
+
+        <md-list slot="md-expand">
+          <md-list-item class="md-inset" to="/scripts">Scripts</md-list-item>
+          <md-list-item class="md-inset" to="/web-tools">Web Tools</md-list-item>
         </md-list>
+      </md-list-item>
+    </md-list>
 
     <md-button class="md-primary md-raised md-dense" @click="logout" v-if="user">Logout</md-button>
   </div>
