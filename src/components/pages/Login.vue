b70ba71c1b81fe8d46931439bdade6e54b80a2a2
Dave Dane Pacilan <davedanepacilan3p@mail.com>
2018-06-12 15:29:02 +0800

    Add Google login method

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -29,6 +29,42 @@
           </md-tab>
 
           <md-tab id="tab-login" md-label="Login">
+            <div class="md-layout md-alignment-top-center text-center">
+              <div class="md-layout-item md-small-hide"></div>
+              <div class="md-layout-item google-login">
+                <p>Log in with your Google Account</p>
+                <button class="btn-google" @click="googleLogin">
+                  <div class="btn-google-wrapper">
+                    <div class="icon-google-wrapper">
+                      <div class="icon-google">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" viewBox="0 0 48 48" class="abcRioButtonSvg">
+                          <g>
+                            <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+                            <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+                            <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+                            <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+                            <path fill="none" d="M0 0h48v48H0z"></path>
+                          </g>
+                        </svg>
+                      </div>
+                    </div>
+                    <span class="btn-google-content">
+                      <span id="not_signed_in8nedvnoy92gx">Sign in with Google</span>
+                    </span>
+                  </div>
+                </button>
+              </div>
+              <div class="md-layout-item md-small-hide"></div>
+            </div>
+
+            <div class="md-layout md-alignment-center-center divider-or text-center">
+              <div class="md-layout-item"><hr></div>
+              <div class="md-layout-item md-size-5">
+                <p class="md-caption">or</p>
+              </div>
+              <div class="md-layout-item"><hr></div>
+            </div>
+
             <md-field :class="{'md-invalid': login.email.error.status}" md-clearable>
               <label>E-mail Address</label>
               <md-input v-model="login.email.value"></md-input>
@@ -234,7 +270,86 @@ export default {
             this.error.message = err.message
           })
       }
+    },
+    googleLogin () {
+      Firebase.auth().signInWithPopup(new Firebase.auth.GoogleAuthProvider()).then(u => {
+        let user = u.user
+        let otherInfo = u.additionalUserInfo
+
+        if (otherInfo.isNewUser) {
+          __DB__.collection('users').doc(user.uid).set({
+            display_name: user.displayName,
+            email: user.email,
+            verified: user.emailVerified,
+            phone_number: user.phoneNumber,
+            photo_URL: user.photoURL,
+            created_at: user.metadata.creationTime,
+            last_signin_at: user.metadata.lastSignInTime,
+            role: 'user'
+          })
+        } else {
+          __DB__.collection('users').doc(user.uid).update({
+            last_signin_at: user.metadata.lastSignInTime
+          })
+        }
+
+        __bus__.$emit('authUser', user)
+
+        this.$router.replace('/home')
+      }).catch(err => {
+        this.error.status = true
+        this.error.message = err.message
+      })
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .text-center {
+    text-align: center;
+  }
+
+  .divider-or hr{
+    border-color: rgba(#fff, .5);
+  }
+
+  .btn-google {
+    width: 240px;
+    height: 50px;
+    border: none;
+    padding: 0;
+    background-color: #4285f4;
+    color: #fff;
+    cursor: pointer;
+
+    &:hover {
+      box-shadow: 0 0 3px 3px rgba(66,133,244,.3);
+    }
+
+    .btn-google-wrapper {
+      border: 1px solid transparent;
+      width: 100%;
+      height: 100%;
+    }
+
+    .icon-google-wrapper {
+      width: 48px;
+      height: 48px;
+      border-radius: 1px;
+      padding: 15px;
+      float: left;
+      background-color: #fff;
+    }
+
+    .icon-google {
+      position: relative;
+    }
+
+    .btn-google-content {
+      vertical-align: top;
+      line-height:48px;
+      font-size:16px;
+    }
+  }
+</style>
