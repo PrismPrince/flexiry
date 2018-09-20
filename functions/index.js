4b10446280287f57a13ad511bc4bbeaf37f0e798
Dave Dane Pacilan <davedanepacilan3p@mail.com>
2018-09-20 10:32:36 +0800

    Update collection and rename "Web Tool" to "Bookmark"

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,11 @@ const functions = require('firebase-functions')
 admin.initializeApp(functions.config().firebase)
 
 var database = admin.firestore()
+database.settings({ timestampsInSnapshots: true })
 
-exports.countWebTool = functions.firestore.document('tools/web/manipulators/{id}').onWrite((change, context) => {
-  var docRef = database.doc('tools/web')
-  var type, all_count, csl_count, cu3_count, mpd_count, pdp_count, trello_count
+exports.countBookmarks = functions.firestore.document('tools/bookmarks/manipulators/{id}').onWrite((change, context) => {
+  var docRef = database.doc('tools/bookmarks')
+  var type, allCount, cslCount, cu3Count, mpdCount, pdpCount, trelloCount
 
   // check create, update, delete
   if (!change.before.exists) {
@@ -15,19 +16,19 @@ exports.countWebTool = functions.firestore.document('tools/web/manipulators/{id}
     type = change.after.data().type
 
     return database.runTransaction(transaction => transaction.get(docRef).then(doc => {
-      all_count = doc.data().all_count + 1
-      csl_count = type.csl ? doc.data().csl_count + 1 : doc.data().csl_count
-      cu3_count = type.cu3 ? doc.data().cu3_count + 1 : doc.data().cu3_count
-      mpd_count = type.mpd ? doc.data().mpd_count + 1 : doc.data().mpd_count
-      pdp_count = type.pdp ? doc.data().pdp_count + 1 : doc.data().pdp_count
-      trello_count = type.trello ? doc.data().trello_count + 1 : doc.data().trello_count
+      allCount = doc.data().allCount + 1
+      cslCount = type.csl ? doc.data().cslCount + 1 : doc.data().cslCount
+      cu3Count = type.cu3 ? doc.data().cu3Count + 1 : doc.data().cu3Count
+      mpdCount = type.mpd ? doc.data().mpdCount + 1 : doc.data().mpdCount
+      pdpCount = type.pdp ? doc.data().pdpCount + 1 : doc.data().pdpCount
+      trelloCount = type.trello ? doc.data().trelloCount + 1 : doc.data().trelloCount
 
-      transaction.update(docRef, { all_count })
-      transaction.update(docRef, { csl_count })
-      transaction.update(docRef, { cu3_count })
-      transaction.update(docRef, { mpd_count })
-      transaction.update(docRef, { pdp_count })
-      transaction.update(docRef, { trello_count })
+      transaction.update(docRef, { allCount })
+      transaction.update(docRef, { cslCount })
+      transaction.update(docRef, { cu3Count })
+      transaction.update(docRef, { mpdCount })
+      transaction.update(docRef, { pdpCount })
+      transaction.update(docRef, { trelloCount })
 
       return console.log('Transaction success')
     })).catch(error => { console.error('Transaction fail:', error) })
@@ -37,17 +38,17 @@ exports.countWebTool = functions.firestore.document('tools/web/manipulators/{id}
     var newType = change.after.data().type
 
     return database.runTransaction(transaction => transaction.get(docRef).then(doc => {
-      csl_count = oldType.csl === newType.csl ? doc.data().csl_count : (!oldType.csl && newType.csl ? doc.data().csl_count + 1 : doc.data().csl_count - 1)
-      cu3_count = oldType.cu3 === newType.cu3 ? doc.data().cu3_count : (!oldType.cu3 && newType.cu3 ? doc.data().cu3_count + 1 : doc.data().cu3_count - 1)
-      mpd_count = oldType.mpd === newType.mpd ? doc.data().mpd_count : (!oldType.mpd && newType.mpd ? doc.data().mpd_count + 1 : doc.data().mpd_count - 1)
-      pdp_count = oldType.pdp === newType.pdp ? doc.data().pdp_count : (!oldType.pdp && newType.pdp ? doc.data().pdp_count + 1 : doc.data().pdp_count - 1)
-      trello_count = oldType.trello === newType.trello ? doc.data().trello_count : (!oldType.trello && newType.trello ? doc.data().trello_count + 1 : doc.data().trello_count - 1)
+      cslCount = oldType.csl === newType.csl ? doc.data().cslCount : (!oldType.csl && newType.csl ? doc.data().cslCount + 1 : doc.data().cslCount - 1)
+      cu3Count = oldType.cu3 === newType.cu3 ? doc.data().cu3Count : (!oldType.cu3 && newType.cu3 ? doc.data().cu3Count + 1 : doc.data().cu3Count - 1)
+      mpdCount = oldType.mpd === newType.mpd ? doc.data().mpdCount : (!oldType.mpd && newType.mpd ? doc.data().mpdCount + 1 : doc.data().mpdCount - 1)
+      pdpCount = oldType.pdp === newType.pdp ? doc.data().pdpCount : (!oldType.pdp && newType.pdp ? doc.data().pdpCount + 1 : doc.data().pdpCount - 1)
+      trelloCount = oldType.trello === newType.trello ? doc.data().trelloCount : (!oldType.trello && newType.trello ? doc.data().trelloCount + 1 : doc.data().trelloCount - 1)
 
-      transaction.update(docRef, { csl_count })
-      transaction.update(docRef, { cu3_count })
-      transaction.update(docRef, { mpd_count })
-      transaction.update(docRef, { pdp_count })
-      transaction.update(docRef, { trello_count })
+      transaction.update(docRef, { cslCount })
+      transaction.update(docRef, { cu3Count })
+      transaction.update(docRef, { mpdCount })
+      transaction.update(docRef, { pdpCount })
+      transaction.update(docRef, { trelloCount })
 
       return console.log('Transaction success')
     })).catch(error => { console.error('Transaction fail:', error) })
@@ -56,29 +57,29 @@ exports.countWebTool = functions.firestore.document('tools/web/manipulators/{id}
     type = change.before.data().type
 
     return database.runTransaction(transaction => transaction.get(docRef).then(doc => {
-      all_count = doc.data().all_count - 1
-      csl_count = type.csl ? doc.data().csl_count - 1 : doc.data().csl_count
-      cu3_count = type.cu3 ? doc.data().cu3_count - 1 : doc.data().cu3_count
-      mpd_count = type.mpd ? doc.data().mpd_count - 1 : doc.data().mpd_count
-      pdp_count = type.pdp ? doc.data().pdp_count - 1 : doc.data().pdp_count
-      trello_count = type.trello ? doc.data().trello_count - 1 : doc.data().trello_count
+      allCount = doc.data().allCount - 1
+      cslCount = type.csl ? doc.data().cslCount - 1 : doc.data().cslCount
+      cu3Count = type.cu3 ? doc.data().cu3Count - 1 : doc.data().cu3Count
+      mpdCount = type.mpd ? doc.data().mpdCount - 1 : doc.data().mpdCount
+      pdpCount = type.pdp ? doc.data().pdpCount - 1 : doc.data().pdpCount
+      trelloCount = type.trello ? doc.data().trelloCount - 1 : doc.data().trelloCount
 
-      transaction.update(docRef, { all_count })
-      transaction.update(docRef, { csl_count })
-      transaction.update(docRef, { cu3_count })
-      transaction.update(docRef, { mpd_count })
-      transaction.update(docRef, { pdp_count })
-      transaction.update(docRef, { trello_count })
+      transaction.update(docRef, { allCount })
+      transaction.update(docRef, { cslCount })
+      transaction.update(docRef, { cu3Count })
+      transaction.update(docRef, { mpdCount })
+      transaction.update(docRef, { pdpCount })
+      transaction.update(docRef, { trelloCount })
 
       return console.log('Transaction success')
     })).catch(error => { console.error('Transaction fail:', error) })
   }
 })
 
-exports.sendCreateWebTool = functions.firestore.document('tools/web/manipulators/{id}').onCreate((snap, context) => {
+exports.sendCreateBookmark = functions.firestore.document('tools/bookmarks/manipulators/{id}').onCreate((snap, context) => {
   var { title, description, version } = snap.data()
   var [major = 0, minor = 0, patch = 0, pre = ''] = version
-  var notif_title = 'Flexiry: New tool added'
+  var notif_title = 'New bookmark added'
   var notif_body = `${title} (v${major}.${minor}.${patch}${pre})\n${description}`
 
   return database.collection('subscribers').get().then(snapshot => snapshot.forEach(doc => {
@@ -88,25 +89,29 @@ exports.sendCreateWebTool = functions.firestore.document('tools/web/manipulators
         title: notif_title,
         body: notif_body,
         icon: '/img/icons/android-chrome-192x192.png',
-        click_action: '/tools/web'
+        click_action: '/bookmarks'
       },
       data: {
         badge: '/img/icons/android-chrome-192x192.png'
       }
     }
 
-    admin.messaging().sendToDevice(token, payload).then(response => response.results.forEach(result => {
-      console.log('Success delivery to', token, result)
+    _sendToDevice(token, payload, doc)
 
-      if (result.error) {
-        console.error('Failed delivery to', token, result.error)
-
-        // Prepare unused tokens for removal
-        if (result.error.code === 'messaging/invalid-registration-token' || result.error.code === 'messaging/registration-token-not-registered') {
-          doc.ref.delete()
-          console.info('Was removed:', token)
-        }
-      } else console.info('Notification sent to', token)
-    })).catch(error => { console.error('Error sending to', token, error) })
   })).catch(error => { console.error('Error getting subscribers', error) })
 })
+
+function _sendToDevice (token, payload, doc) {
+  admin.messaging().sendToDevice(token, payload).then(response => response.results.forEach(result => {
+    console.log('Success delivery to', token, result)
+
+    if (result.error) {
+      console.error('Failed delivery to', token, result.error)
+
+      if (result.error.code === 'messaging/invalid-registration-token' || result.error.code === 'messaging/registration-token-not-registered') {
+        doc.ref.delete()
+        console.info('Was removed:', token)
+      }
+    } else console.info('Notification sent to', token)
+  })).catch(error => { console.error('Error sending to', token, error) })
+}
