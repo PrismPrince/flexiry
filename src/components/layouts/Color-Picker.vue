21231b679fee37d2e5457a8f9fd5991e0d842e64
Dave Dane Pacilan <davedanepacilan3p@mail.com>
2018-12-04 12:42:59 +0800

    Add Color-Picker component

---
+++ src/components/layouts/Color-Picker.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-menu v-model="baseColorMenu" :min-width="minWidth" :nudge-left="nudgeLeft" :nudge-bottom="nudgeBottom" :offset-x="offsetX" :offset-y="offsetY">
+    <slot name="activator" slot="activator"></slot>
+    <v-list dense style="max-height: 528px;">
+      <v-menu v-for="(color, key) in colors" :key="color.text" offset-x left open-on-hover full-width>
+        <v-list-tile slot="activator" :class="color.text === 'shades' ? 'white' : color.text" @click="select(color.hex !== null ? color.hex : '#ffffff')" style="width: 100%">
+          <v-list-tile-title>{{ color.color }}</v-list-tile-title>
+          <v-list-tile-action class="justify-end">
+            <v-list-tile-action-text class="white--text">{{ color.hex }}</v-list-tile-action-text>
+          </v-list-tile-action>
+        </v-list-tile>
+
+        <v-list dense>
+          <v-list-tile v-for="(alt, i) in color.alt" :key="alt.text" :class="alt.text" @click="select(alt.hex)">
+            <v-list-tile-title :class="alt.text.match(/(darken)|(black)/) ? 'white--text' : ''">{{ alt.color }}</v-list-tile-title>
+            <v-list-tile-action class="justify-end">
+              <v-list-tile-action-text :class="alt.text.match(/(darken)|(black)/) ? 'white--text' : 'black--text'">{{ alt.hex !== 'transparent' ? alt.hex : '' }}</v-list-tile-action-text>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+import colors from '@/mixins/colors'
+
+export default {
+  name: 'color-picker',
+  props: {
+    minWidth: {
+      type: [String, Number],
+      default: undefined
+    },
+    nudgeLeft: {
+      type: [String, Number],
+      default: 0
+    },
+    nudgeBottom: {
+      type: [String, Number],
+      default: 0
+    },
+    offsetX: {
+      type: Boolean,
+      default: false
+    },
+    offsetY: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      colors,
+      baseColorMenu: false
+    }
+  },
+  methods: {
+    select (hex) {
+      this.$emit('select', hex)
+      this.baseColorMenu = false
+    }
+  }
+}
+</script>
