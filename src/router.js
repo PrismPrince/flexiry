af7074d9c305b23396d74faff5f2ad7994199abc
Dave Dane Pacilan <davedanepacilan3p@mail.com>
2019-03-15 06:32:47 +0800

    Add Font List route

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import Welcome from './views/Welcome'
 import Bookmarks from './views/Bookmarks'
 import FileAudit from './views/File-Audit'
 import FontSwaps from './views/Font-Swaps'
+import FontList from './views/Font-List'
 import Imgur from './views/Imgur'
 
 const database = Firebase.firestore()
@@ -82,6 +83,11 @@ let router = new Router({
       path: '/imgur',
       name: 'Imgur',
       component: Imgur
+    },
+    {
+      path: '/font-list',
+      name: 'FontList',
+      component: FontList
     }
     // {
     //   path: '/qc-rejection',
